d2107bd80517b6766f5a43a3462b8722090ff455
tarun <tarunlal07@gmail.com>
2025-04-20 03:04:46 +0530

    fix/refactor

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -91,7 +91,7 @@ export default function LoginPage() {
                 href="/register" 
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
-                Don't have an account? Sign up
+                Don&apos;t have an account? Sign up
               </Link>
             </div>
           </div>
